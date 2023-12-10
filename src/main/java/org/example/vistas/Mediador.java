d4dc3c8b1b35fd0cc567e1cb6e0957b1cb018950
Diego-ipt <106196745+Diego-ipt@users.noreply.github.com>
2023-12-10 15:36:25 -0300

    avance en el esqueleto de PanelCompra

--- src/main/java/org/example/vistas/Mediador.java
+++ src/main/java/org/example/vistas/Mediador.java
@@ -1,13 +1,16 @@
 package org.example.vistas;
 import org.example.modelos.*;
 
+import java.util.ArrayList;
+
 public class Mediador {
     private PanelFechaDestino panelFechaDestino;
     private PanelHorarios panelHorarios;
     private PanelCompra panelcompra;
     private FechasYTramo fechaytramo;
     private Bus bus;
-    private Pasaje pasaje;
+    private ArrayList<Asiento> Asientos;
+    private String hora;
     public Mediador(){
     }
     public void refresh(FechasYTramo fechaytramo){
@@ -15,10 +18,11 @@ public class Mediador {
         this.panelHorarios.setFechaYTramo(fechaytramo);
     }
     public void refresh(String hora) throws HorarioNoDisponibleException {
+        this.hora=hora;
         this.bus=this.fechaytramo.getBus(hora);
         this.panelcompra.setbus(bus);
     }
-    public void refresh(Pasaje pasaje){
+    public void refresh(ArrayList<Asiento> Asientos){
         //this.pasaje=pasaje;
         //this.panelpasaje.setpasaje(pasaje);
     }
@@ -26,5 +30,11 @@ public class Mediador {
         this.panelcompra= panelcompra;
         this.panelHorarios=panelHorarios;
         this.panelFechaDestino=panelFechaDestino;
+    }
+    public FechasYTramo getFechaytramo() {
+        return this.fechaytramo;
+    }
+    public String getHora() {
+        return this.hora;
     }
 }
